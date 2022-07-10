da5a9d8464c11c937afa2ee42cb64d6abbd80d9b
MrCronkite <vladshima99@gmail.com>
2022-07-10 20:17:06 +0300

    mvc

--- MVC/MVC/ViewController.swift
+++ MVC/MVC/ViewController.swift
@@ -8,10 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+   
+    
+   override func loadView(){
+        super.loadView()
+        print("loadView")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        hello()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+          super.viewDidAppear(animated)
+        print("viewDidApper")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillApper")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("viewDidLayoutSubviews")
+    }
+    
+    func hello(){
+        print("hello")
     }
 
 
