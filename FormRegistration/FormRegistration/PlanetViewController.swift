acf9cc58936c4b4555dfb857655c66f5b5ccd6ff
MrCronkite <vladshima99@gmail.com>
2022-08-31 22:20:54 +0300

    get request

--- FormRegistration/FormRegistration/PlanetViewController.swift
+++ FormRegistration/FormRegistration/PlanetViewController.swift
@@ -17,9 +17,26 @@ class PlanetViewController: UIViewController {
     }
 
     
-    func getImagePlanet() {
-        let url = "https://api.nasa.gov/planetary/apod?api_key=\(key)"
+    @IBAction func GET(_ sender: Any) {
+        guard let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=\(key)") else { return }
         
-        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if let response = response {
+                print(response)
+            }
+            
+            guard let data = data else { return }
+                print( data )
+            
+            do{
+                let json =  try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+            
+        }.resume()
     }
 }
+
