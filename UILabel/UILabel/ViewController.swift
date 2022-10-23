e750bc1da0f072d1dcf0d1e13a47d0f322e19ef9
MrCronkite <vladshima99@gmail.com>
2022-10-23 21:06:07 +0300

    uilabel

---
+++ UILabel/UILabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UILabel
+//
+//  Created by admin1 on 23.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
