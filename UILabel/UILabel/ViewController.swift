55ef241ac405a4e285e92006edf0854b87016b04
MrCronkite <vladshima99@gmail.com>
2022-10-23 21:17:25 +0300

    uilabel

--- UILabel/UILabel/ViewController.swift
+++ UILabel/UILabel/ViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        label.frame = CGRect(x: 100, y: 300, width: 200, height: 150)
+        label.text = "some text for hello world vladik 12345888"
+        label.textColor = .link
+        label.numberOfLines = 0
+        
+        label.shadowColor = .green
+        label.shadowOffset = CGSize(width: 1, height: 1)
+        view.addSubview(label)
+        
        
     }
 
