62c72e08039b577e159d9e8decd88728caf94ce5
MrCronkite <vladshima99@gmail.com>
2022-10-24 22:39:30 +0300

    uibutton

--- UIButton/UIButton/ViewController.swift
+++ UIButton/UIButton/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        button.frame = CGRect(x: 100, y: 100, width: 50, height: 100)
+        button.setTitle("hello", for: .normal )
+        button.setTitleColor( .darkGray, for: .normal)
+        button.backgroundColor = .green
+        view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(getText), for: .touchUpInside)
     }
 
 
+    @objc func getText(){
+        print("hello")
+    }
 }
 
