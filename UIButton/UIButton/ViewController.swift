e08ca1425a77c8ab189c77ed798e483d1e3e7daa
MrCronkite <vladshima99@gmail.com>
2022-10-26 20:10:46 +0300

    constraints

--- UIButton/UIButton/ViewController.swift
+++ UIButton/UIButton/ViewController.swift
@@ -13,20 +13,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         button.setTitle("hello", for: .normal )
         button.setTitleColor( .darkGray, for: .normal)
         button.backgroundColor = .green
+        view.addSubview(button)
         
         button.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            button.widthAnchor.constraint(equalToConstant: 150),
             button.heightAnchor.constraint(equalToConstant: 100),
-           // button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -6),
-           // button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
+            button.widthAnchor.constraint(equalToConstant: 100),
+            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 56),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
-        
-        view.addSubview(button)
         
         button.addTarget(self, action: #selector(getText), for: .touchUpInside)
     }
