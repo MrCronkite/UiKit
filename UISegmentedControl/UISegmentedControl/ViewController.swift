069910831e8279531fd54d07ba1cd4cba07059da
MrCronkite <vladshima99@gmail.com>
2022-10-27 20:57:15 +0300

    UIsegmendetСontrol

---
+++ UISegmentedControl/UISegmentedControl/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  UISegmentedControl
+//
+//  Created by admin1 on 27.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let segmented = UISegmentedControl()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        segmented.insertSegment(withTitle: "one", at: 1, animated: true)
+        segmented.insertSegment(withTitle: "two", at: 2, animated: true)
+        segmented.insertSegment(withTitle: "three", at: 0, animated: true)
+        
+        view.addSubview(segmented)
+        
+        
+        
+    }
+
+
+}
+
