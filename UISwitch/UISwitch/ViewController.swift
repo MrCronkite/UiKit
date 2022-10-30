a898f7cea8d3acbc416af3dce77f38b6e6b1e6a9
MrCronkite <vladshima99@gmail.com>
2022-10-30 21:01:01 +0300

    UISwitch

---
+++ UISwitch/UISwitch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISwitch
+//
+//  Created by admin1 on 30.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
