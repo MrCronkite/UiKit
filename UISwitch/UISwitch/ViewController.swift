f7fc4d289bef4e2e2342c381e93f9c546b96f4cc
MrCronkite <vladshima99@gmail.com>
2022-10-30 21:05:25 +0300

    UISwitch

--- UISwitch/UISwitch/ViewController.swift
+++ UISwitch/UISwitch/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let switchView = UISwitch()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(switchView)
+        switchView.setOn(true, animated: true)
+        
+        
+        switchView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+           // switchView.heightAnchor.constraint(equalToConstant: 200),
+           // switchView.widthAnchor.constraint(equalToConstant: 200),
+            switchView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            switchView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        
     }
 
 
