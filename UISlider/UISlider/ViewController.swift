1bd8ea983a7c375f5d041c0c18cfcb727dd7547c
MrCronkite <vladshima99@gmail.com>
2022-11-02 22:13:19 +0300

    uISlider

--- UISlider/UISlider/ViewController.swift
+++ UISlider/UISlider/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let slider = UISlider(frame: CGRect(x: 0, y: 0, width: 300, height: 4))
+        view.addSubview(slider)
+        slider.center = view.center
+
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.addTarget(self, action: #selector(sliderChange), for: .valueChanged)
+        //slider.isContinuous = false
+        
+        label.frame = CGRect(x: 30, y: 300, width: 100, height: 100)
+        view.addSubview(label)
+        label.text = "Ghbdtn"
+        
     }
 
-
+    @objc func sliderChange(sender: UISlider){
+        label.text = "\(sender.value)"
+    }
 }
 
