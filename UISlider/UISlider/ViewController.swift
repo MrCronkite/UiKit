33e2c8debfd8bdb156cc3ec6f2ef8d37e1fec959
MrCronkite <vladshima99@gmail.com>
2022-11-02 22:17:34 +0300

    uISlider

--- UISlider/UISlider/ViewController.swift
+++ UISlider/UISlider/ViewController.swift
@@ -16,14 +16,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        let slider = UISlider(frame: CGRect(x: 0, y: 0, width: 300, height: 4))
+        let slider = UISlider(frame: CGRect(x: 0, y: 0, width: 300, height: 20))
         view.addSubview(slider)
         slider.center = view.center
 
         slider.minimumValue = 0
         slider.maximumValue = 100
         slider.addTarget(self, action: #selector(sliderChange), for: .valueChanged)
-        //slider.isContinuous = false
+        slider.isContinuous = false
+        
+        slider.thumbTintColor = .red
+        slider.maximumTrackTintColor = .yellow
+        slider.minimumTrackTintColor = .green
         
         label.frame = CGRect(x: 30, y: 300, width: 100, height: 100)
         view.addSubview(label)
