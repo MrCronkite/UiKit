5cd8e3d306c5a99f4185eee79982d7d906e249a6
MrCronkite <vladshima99@gmail.com>
2022-11-06 19:59:38 +0300

    UIPickerView

--- UIPickerView/UIPickerView/ViewController.swift
+++ UIPickerView/UIPickerView/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let picker = UIPickerView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        picker.frame = CGRect(x: 10, y: 100, width: 340, height: 216)
+        view.addSubview(picker)
+        picker.delegate = self
+        picker.dataSource = self
     }
 
+}
 
+extension ViewController: UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        4
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        20
+    }
+    
+    
+}
+
+extension ViewController: UIPickerViewDelegate{
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(row)"
+    }
 }
 
