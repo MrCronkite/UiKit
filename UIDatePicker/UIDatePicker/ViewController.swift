2e5f40abbda12bea0607cd295e2d341cf88a813a
MrCronkite <vladshima99@gmail.com>
2022-11-07 08:52:16 +0300

    UIPickerView

---
+++ UIDatePicker/UIDatePicker/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UIDatePicker
+//
+//  Created by admin1 on 7.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let datePicker = UIDatePicker()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
