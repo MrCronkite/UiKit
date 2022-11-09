8df78c6a8ac9c104ba2bae845d0b2caf2822071f
MrCronkite <vladshima99@gmail.com>
2022-11-09 19:53:10 +0300

    UIPickerView

---
+++ UIStepper/UIStepper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIStepper
+//
+//  Created by admin1 on 9.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
