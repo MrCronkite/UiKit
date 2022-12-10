98d67870ca36cb58d8f06d29112bbc1862a728aa
MrCronkite <vladshima99@gmail.com>
2022-12-10 20:34:50 +0300

    liveActivity

---
+++ LiveActivity/LiveActivity/LiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityApp.swift
+//  LiveActivity
+//
+//  Created by admin1 on 10.12.22.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiveActivity/OrderStatus/OrderStatusBundle.swift
@@ -0,0 +1,17 @@
+//
+//  OrderStatusBundle.swift
+//  OrderStatus
+//
+//  Created by admin1 on 10.12.22.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct OrderStatusBundle: WidgetBundle {
+    var body: some Widget {
+        OrderStatus()
+        OrderStatusLiveActivity()
+    }
+}
