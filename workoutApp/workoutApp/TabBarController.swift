e5a67e9fce9b56cb2ffe5865dc097d6376bc4bba
MrCronkite <vladshima99@gmail.com>
2023-03-07 22:39:39 +0300

    add new progect

---
+++ workoutApp/workoutApp/TabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  TabBarController.swift
+//  workoutApp
+//
+//  Created by admin1 on 7.03.23.
+//
+
+import UIKit
+
+enum Tabs: Int {
+    case overview
+    case session
+    case progress
+    case settings
+}
+
+final class TabBarController: UITabBarController {
+    
+    
+    private func configure() {
+        
+    }
+}
