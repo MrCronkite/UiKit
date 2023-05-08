3fea96e2e560ea17b9013853a7eeb949ff71e0fc
MrCronkite <vladshima99@gmail.com>
2023-05-08 21:24:23 +0300

    storage bg fix

--- workoutApp/workoutApp/Controllers/Progress/ProgressController.swift
+++ workoutApp/workoutApp/Controllers/Progress/ProgressController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class ProgressController: BaseController {
     
-    private let dailyPerformanceView = DailyPerformanceView()
+    private let dailyPerformanceView = DailyPerformanceView(with: R.Strings.Progress.dailyPerformance,
+                                                            buttonTitle: R.Strings.Progress.last7Days.uppercased())
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +20,18 @@ class ProgressController: BaseController {
 extension ProgressController {
     override func setupViews() {
         super.setupViews()
+        
+        view.addView(dailyPerformanceView)
     }
     
     override func constraintViews() {
         super.constraintViews()
+        
+        NSLayoutConstraint.activate([
+            dailyPerformanceView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            dailyPerformanceView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            dailyPerformanceView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15)
+        ])
     }
     
     override func configureView() {
