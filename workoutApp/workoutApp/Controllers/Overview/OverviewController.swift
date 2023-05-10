4a4a7c04e81349978fd6eddba06a715769fb13ad
MrCronkite <vladshima99@gmail.com>
2023-05-10 22:54:30 +0300

    storage bg fix

--- workoutApp/workoutApp/Controllers/Overview/OverviewController.swift
+++ workoutApp/workoutApp/Controllers/Overview/OverviewController.swift
@@ -11,15 +11,17 @@ class OverviewController: BaseController {
     
     private let navBar = OverviewNavBar()
     
-    private let header = SectionHeaderView()
-    
-    private let cell = TrainingCellView()
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
         
-        cell.roundCorners([.allCorners], radius: 5)
-    }
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.showsVerticalScrollIndicator = false
+        view.backgroundColor = .clear
+        
+        return view
+    }()
+    
 }
 
 extension OverviewController {
@@ -27,8 +29,7 @@ extension OverviewController {
         super.setupViews()
         
         view.addView(navBar)
-        view.addView(header)
-        view.addView(cell)
+        view.addView(collectionView)
     }
     
     override func constraintViews() {
@@ -39,16 +40,10 @@ extension OverviewController {
             navBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             navBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
-            header.topAnchor.constraint(equalTo: navBar.bottomAnchor),
-            header.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            header.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            header.heightAnchor.constraint(equalToConstant: 32),
-            
-            cell.topAnchor.constraint(equalTo: header.bottomAnchor),
-            cell.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            cell.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            cell.heightAnchor.constraint(equalToConstant: 75)
-        
+            collectionView.topAnchor.constraint(equalTo: navBar.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
     
@@ -58,11 +53,50 @@ extension OverviewController {
         
         let dateFormater = DateFormatter()
         dateFormater.dateFormat = "EEEE, MMMM dd"
+//
+//        header.configure(with: dateFormater.string(from: Date()))
+//
+//        cell.configure(with: "Warm Up Cardio", subtitle: "asasasasas", isDone: true)
+//
+        collectionView.register(TrainingCellView.self, forCellWithReuseIdentifier: TrainingCellView.id)
+        collectionView.register(SectionHeaderView.self, forSupplementaryViewOfKind:
+                                    UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: SectionHeaderView.id)
         
-        header.configure(with: dateFormater.string(from: Date()))
-        
-        cell.configure(with: "Warm Up Cardio", subtitle: "asasasasas", isDone: true)
-        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 }
 
+extension OverviewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: 70)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        referenceSizeForHeaderInSection section: Int) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: 70)
+    }
+    
+}
+
+extension OverviewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TrainingCellView.id,
+                                                            for: indexPath) as? TrainingCellView else { return UICollectionViewCell() }
+        
+        cell.configure(with: "text", subtitle: "wrwew", isDone: true)
+        return cell
+    }
+    
+    
+}
